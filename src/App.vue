1d06e0d50d09d572928a66d8c286639109d4779f
victor-de-sousa <victordesousaestudo@gmail.com>
2025-10-27 10:31:52 -0300

    feat: finished the header

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Início</a>
+        </li>
+        <li>
+          <a href="#">Trajetória</a>
+        </li>
+        <li>
+          <a href="#">Habilidades</a>
+        </li>
+        <li>
+          <a href="#">Formação</a>
+        </li>
+      </ul>
+    </nav>
+
+    <section class="inicio">
+      <h1>Início</h1>
+    </section>
+
+  </main>
+</template>
+
+<style scoped>
+
+
+  nav ul {
+    display: flex;
+    justify-content: space-between;
+    margin: 2rem 65vh 1rem 65vh;
+    font-size: 1.3rem;
+  }
+
+  a {
+    color: black;
+  }
+
+</style>
